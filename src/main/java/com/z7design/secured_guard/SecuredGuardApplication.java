4c579baf010be10f2939efd774ac4e0a5c21156a
josemario74 <z7designcode>
2025-05-22 15:14:18 -0300

    Initial commit: Project setup with Spring Boot

---
+++ src/main/java/com/z7design/secured_guard/SecuredGuardApplication.java
@@ -0,0 +1,13 @@
+package com.z7design.secured_guard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredGuardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/z7design/secured_guard/SecuredGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z7design.secured_guard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
